908073de9eaf2b2564ea6462857defff70c12432
su8 <wifiextender2@gmail.com>
2025-11-05 09:17:06 +0300

    use emplace_back over push_back

--- main.cpp
+++ main.cpp
@@ -15,7 +15,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 MA 02110-1301, USA.
 */
-
 #include <iostream>
 #include <cstdio>
 #include <cstring>
@@ -28,8 +27,20 @@ MA 02110-1301, USA.
 
 std::vector<std::string> variants(const std::string &word);
 
+std::vector<std::string> variants(const std::string &word) {
+  std::vector<std::string> result;
+  result.emplace_back(word);
+  std::string capitalized = word;
+  if (!capitalized.empty()) { capitalized[0] = static_cast<char>(std::toupper(capitalized[0])); }
+  result.emplace_back(capitalized);
+  std::string upper = word;
+  std::transform(upper.begin(), upper.end(), upper.begin(), [](unsigned char ch) { return std::toupper(ch); } );
+  result.emplace_back(upper);
+  return result;
+}
+
 int main(int argc, char *argv[]) {
-  if (argc < 4) { return EXIT_SUCCESS; }
+  if (argc < 6) { return EXIT_SUCCESS; }
   std::vector<std::string> usernames = {"frost"};
   std::vector<std::string> roles = {"user", "root"};
   std::vector<std::string> separators = {"", "_"};
@@ -38,28 +49,11 @@ int main(int argc, char *argv[]) {
   for (const auto &x : usernames) {
     for (const auto &z : roles) {
       for (const auto &sep : separators) {
-        // Iterate over all combinations
-        for (const auto &user : variants(x)) {
+        for (const auto &user : variants(x)) { // Iterate over all combinations
           for (const auto &role : variants(z)) { std::cout << user << sep << role << std::endl; }
         }
       }
     }
   }
   return EXIT_SUCCESS;
-}
-
-std::vector<std::string> variants(const std::string &word) {
-  std::vector<std::string> result;
-  result.push_back(word);
-
-  // Capitalize the first letter of the word
-  std::string capitalized = word;
-  if (!capitalized.empty()) { capitalized[0] = static_cast<char>(std::toupper(capitalized[0])); }
-  result.push_back(capitalized);
-
-  // Convert the word to uppercase
-  std::string upper = word;
-  std::transform(upper.begin(), upper.end(), upper.begin(), [](unsigned char ch) { return std::toupper(ch); } );
-  result.push_back(upper);
-  return result;
 }
